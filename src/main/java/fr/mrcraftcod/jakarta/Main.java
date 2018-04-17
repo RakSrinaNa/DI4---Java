ce16e89c5d8eb418e1e2f8dcf09e7e5a276941ad
Thomas COUCHOUD <zerderr@gmail.com>
2018-04-17 10:36:15 +0200

    Base prog

---
+++ src/main/java/fr/mrcraftcod/jakarta/Main.java
@@ -0,0 +1,15 @@
+package fr.mrcraftcod.jakarta;
+
+/**
+ * Created by Thomas Couchoud (MrCraftCod - zerderr@gmail.com) on 17/04/2018.
+ *
+ * @author Thomas Couchoud
+ * @since 2018-04-17
+ */
+public class Main
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
