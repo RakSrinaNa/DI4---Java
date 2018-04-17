bf5c4fe7bbd80c0821b35552b5fc5c1b9bb2e7e3
Thomas COUCHOUD <zerderr@gmail.com>
2018-04-17 12:25:05 +0200

    No crash no stress

--- src/main/java/com/codetroopers/eput/services/AuthenticationService.java
+++ src/main/java/com/codetroopers/eput/services/AuthenticationService.java
@@ -1,8 +1,6 @@
 package com.codetroopers.eput.services;
 
-
 import com.codetroopers.eput.models.UserInfo;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
@@ -10,43 +8,50 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
 
-
 @Named
 @ApplicationScoped
-public class AuthenticationService implements Serializable {
-
-    @Inject
-    FacesContext facesContext;
-
-    /**
-     * Login method checking if the supplied userInfo matches our condition on its password/nickname combination :
-     * <p>
-     * As a side effect, it sets the flag userInfo#loggedIn with the result of the check
-     *
-     * @param userInfo userInfo to check
-     * @return next view to show / null if a refresh is needed
-     */
-    public String login(final UserInfo userInfo) {
-        if ("tpjava".equals(userInfo.getName()) && "secret".equals(userInfo.getPassword())) {
-            userInfo.setLoggedIn(true);
-            return "success";
-        }
-        facesContext.addMessage("Invalid credentials", new FacesMessage("Invalid credentials !"));
-        return null;
-    }
-
-    /**
-     * Method to invalidate current user session. It relies on invalidating user's session from FacesContext
-     *
-     * @return next path (with redirect)
-     */
-    public String logout() {
-        facesContext.getExternalContext().invalidateSession();
-        return "login" + "?faces-redirect=true";
-    }
-
-    String login(final String nickName, final String password) {
-        boolean result = false;
-        return null;
-    }
+public class AuthenticationService implements Serializable
+{
+	
+	@Inject
+	FacesContext facesContext;
+	
+	/**
+	 * Login method checking if the supplied userInfo matches our condition on its password/nickname combination :
+	 * <p>
+	 * As a side effect, it sets the flag userInfo#loggedIn with the result of the check
+	 *
+	 * @param userInfo userInfo to check
+	 *
+	 * @return next view to show / null if a refresh is needed
+	 */
+	public String login(final UserInfo userInfo)
+	{
+		if("tpjava".equals(userInfo.getName()) && "secret".equals(userInfo.getPassword()))
+		{
+			userInfo.setLoggedIn(true);
+			System.out.println("LOGGED");
+			return "success";
+		}
+		System.out.println("NOT LOGGED");
+		facesContext.addMessage("Invalid credentials", new FacesMessage("Invalid credentials !"));
+		return null;
+	}
+	
+	/**
+	 * Method to invalidate current user session. It relies on invalidating user's session from FacesContext
+	 *
+	 * @return next path (with redirect)
+	 */
+	public String logout()
+	{
+		facesContext.getExternalContext().invalidateSession();
+		return "login" + "?faces-redirect=true";
+	}
+	
+	String login(final String nickName, final String password)
+	{
+		boolean result = false;
+		return null;
+	}
 }
